152a6fe150a5abf5fac8f70b3d9baf24e4e94fca
juan.ramirez.q <juan.ramirez.q@e1c209e8-38ec-11de-a996-7ff70a941223>
2009-05-06 14:49:28 +0000

    Ventana con mascara y con fondo

---
+++ MusicP/src/app.java
@@ -0,0 +1,29 @@
+import interfaz.Ventana;
+
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+import com.sun.jna.examples.WindowUtils;
+
+
+public class app {
+
+	public static void main(String[] args) {
+		System.setProperty("sun.java2d.noddraw", "true");
+		try {
+			UIManager.setLookAndFeel(new WindowsLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		Shape mask=new RoundRectangle2D.Float(10, 40, 526, 314, 118, 118);
+		Ventana miVentana=new Ventana();
+		WindowUtils.setWindowMask(miVentana, mask);
+
+		miVentana.setVisible(true);
+	}
+
+}
