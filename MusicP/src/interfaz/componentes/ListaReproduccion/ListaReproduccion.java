209813c95e3f68795dd05fa0a7aa7357e641e9d3
juan.ramirez.q <juan.ramirez.q@e1c209e8-38ec-11de-a996-7ff70a941223>
2009-05-16 18:50:45 +0000

    Panel con Lista de reproduccion medio listo, falta partes funcionales

--- MusicP/src/interfaz/componentes/ListaReproduccion/ListaReproduccion.java
+++ MusicP/src/interfaz/componentes/ListaReproduccion/ListaReproduccion.java
@@ -2,12 +2,15 @@ package interfaz.componentes.ListaReproduccion;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
-import javax.swing.JList;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import logica.Cancion;
@@ -20,14 +23,17 @@ public class ListaReproduccion extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JTextField entrada;
 	private BotonHerramienta buscar;
-	private JList lista;
+	private BotonHerramienta agregar;
+	private JPanel lista;
 	private ArrayList<Cancion> listaCanciones;
+	private ArrayList<JPanel> canciones;
 	private JPanel barraHerramientas;
 
 	public ListaReproduccion() {
 		this.setOpaque(false);
 		this.setLayout(new BorderLayout());
-
+		listaCanciones = new ArrayList<Cancion>();
+		canciones = new ArrayList<JPanel>();
 		inicializarComponentes();
 	}
 
@@ -45,10 +51,18 @@ public class ListaReproduccion extends JPanel {
 		buscar.setText("Buscar");
 		barraHerramientas.add(buscar);
 
+		agregar = new BotonHerramienta();
+		agregar.setText("Agregar");
+		barraHerramientas.add(agregar);
+
 		this.add(barraHerramientas, BorderLayout.NORTH);
 
-		lista = new JList();
-		this.add(lista, BorderLayout.CENTER);
+		lista = new JPanel();
+		lista.setOpaque(false);
+		BoxLayout bl = new BoxLayout(lista, BoxLayout.Y_AXIS);
+		lista.setLayout(bl);
+		JScrollPane js = new JScrollPane(lista);
+		this.add(js, BorderLayout.CENTER);
 	}
 
 	public ArrayList<Cancion> getListaCanciones() {
@@ -57,6 +71,24 @@ public class ListaReproduccion extends JPanel {
 
 	public void setListaCanciones(ArrayList<Cancion> listaCanciones) {
 		this.listaCanciones = listaCanciones;
+		canciones = new ArrayList<JPanel>();
+		for (int j = 0; j < listaCanciones.size(); j++) {
+			Cancion i=listaCanciones.get(j);
+			JPanel c = new JPanel(new BorderLayout());
+			if(j%2==0){
+				c.setBackground(Color.BLACK);
+			} else{
+				c.setBackground(Color.DARK_GRAY);
+			}
+
+			JLabel l = new JLabel(i.getNombre()+" - "+i.getArtista());
+			l.setForeground(Color.WHITE);
+			c.add(l, BorderLayout.CENTER);
+			c.setMaximumSize(new Dimension(800, 20));
+
+			canciones.add(c);
+			lista.add(c);
+		}
 	}
 
 	@Override
